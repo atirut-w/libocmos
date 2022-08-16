dbe21ecf10601f89ef296e4114c351100e6869d4
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-08-17 00:47:59 +0700

    Some MMU stuff

---
+++ src/include/ocmos/mmu.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <stdint.h>
+
+int get_bank_count();
---
+++ src/mmu.c
@@ -0,0 +1,6 @@
+#include <ocmos/mmu.h>
+
+int get_bank_count()
+{
+    return (*(uint8_t*)0x210 << 8) | *(uint8_t*)0x200;
+}
