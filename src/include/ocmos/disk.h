322aad53308a56250039d1cf7a0dbf732123c70f
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-08-17 21:42:41 +0700

    Update some comments

--- src/include/ocmos/disk.h
+++ src/include/ocmos/disk.h
@@ -6,11 +6,11 @@
 
 typedef struct disk
 {
-    uint16_t uuid;      // First two bytes of the disk's UUID
-    uint8_t sectorsize; // Sector size in units of 256 bytes
-    uint8_t rw;         // Serial read/write port
-    uint16_t size;      // Size of the disk in units of sectors
-    uint16_t sector;    // Current sector for read/write
+    uint16_t uuid;      // First two bytes of the disk's UUID.
+    uint8_t sectorsize; // Sector size in units of 256 bytes.
+    uint8_t rw;         // Serial read/write port.
+    uint16_t size;      // Size of the disk in units of sectors.
+    uint16_t sector;    // Current sector for read/write. Starts at 1.
 } disk_t;
 
 void read_sector(disk_t *disk, uint8_t *buffer);
